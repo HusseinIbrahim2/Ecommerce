bffc5ed369d214c53c74d7eb7fa4b32ce3aab6d3
HusseinIbrahim2 <husseinib23920@gmail.com>
2024-12-07 23:50:29 +0100

    Create the backend folder

--- apps/ecom-frontend/src/app/layout/footer/footer.component.ts
+++ apps/ecom-frontend/src/app/layout/footer/footer.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faTruckFast } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'ecom-footer',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  private faIconLibrary = inject(FaIconLibrary);
+
+  constructor() {
+    this.faIconLibrary.addIcons(
+      faFacebook,
+      faTwitter,
+      faYoutube,
+      faTruckFast
+    );
+  }
+}
